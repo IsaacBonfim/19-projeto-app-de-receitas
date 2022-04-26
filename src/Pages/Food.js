71b70b1901912184aeb5a839794bc7b0ccc9bb20
Ligia A. Constantino <cellerligia23@gmail.com>
2022-04-26 17:36:43 -0300

    Tela de Login - Requisitos 1, 2, 3, 4, 5 e 8 feitos

---
+++ src/Pages/Food.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Food() {
+  return (
+    <div>
+      <h1>Página de Comida</h1>
+    </div>
+  );
+}
+
+export default Food;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AppProvider from './Context/AppProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AppProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AppProvider>, document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
