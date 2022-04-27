db9b0610e9265684b8b70fbf34bb6e522bd9b900
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-04-27 14:53:25 -0300

    Iniciando o desenvolvimento do Hearder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Routes from './Routes';
+import './Styles/App.css';
 
 function App() {
   return (
-    <div>
-      <Routes />
-    </div>
+    <Routes />
   );
 }
 
