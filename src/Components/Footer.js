b308e899b23baad8b4b0d3d8ab9189211162c628
isagomachado <isa_gomes_machado@hotmail.com>
2022-04-28 18:20:27 -0300

    Requisito 20, 21, 22, 23 e 24

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 
+import '../Styles/Footer.css';
+
 function Footer() {
+  const history = useHistory();
+
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="fixarRodape">
       <button
         type="button"
         data-testid="drinks-bottom-btn"
         src={ drinkIcon }
         alt="Drink Icon"
+        onClick={ () => history.push('/drinks') }
       >
         <img src={ drinkIcon } alt="Drink Icon" />
       </button>
@@ -21,6 +27,7 @@ function Footer() {
         data-testid="explore-bottom-btn"
         src={ exploreIcon }
         alt="Explore Icon"
+        onClick={ () => history.push('/explore') }
       >
         <img src={ exploreIcon } alt="Explore Icon" />
       </button>
@@ -30,6 +37,7 @@ function Footer() {
         type="button"
         src={ mealIcon }
         alt="Meal Icon"
+        onClick={ () => history.push('/foods') }
       >
         <img src={ mealIcon } alt="Meal Icon" />
       </button>
