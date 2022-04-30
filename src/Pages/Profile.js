5a6a6e7a655ccb125f2b68a4a3211bdef4039587
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-04-30 00:21:03 -0300

    Requisitos da Tela de Perfil

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import '../Styles/Profile.css';
 
 function Profile() {
+  const history = useHistory();
+
+  let user = localStorage.getItem('user');
+  user = JSON.parse(user);
+
   return (
     <>
       <Header title="Profile" />
-      <h1>Perfil</h1>
+      <section className="profile-container">
+        <span
+          data-testid="profile-email"
+        >
+          { user.email }
+        </span>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          Logout
+        </button>
+      </section>
       <Footer />
     </>
   );
