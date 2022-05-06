2578075e3133d48d47c246268b2e1e6a9ace841c
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-05-06 18:09:42 -0300

    CSS da página de Not found

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../Styles/NotFound.css';
 
 function NotFound() {
   return (
-    <h1>
-      Not Found
-    </h1>
+    <div className="not-container">
+      <h1 className="top-text">Sorry,</h1>
+      <h1 className="middle-text">this option is</h1>
+      <h1 className="not-found">
+        Not Found
+      </h1>
+    </div>
   );
 }
 
